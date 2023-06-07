f55df786828c56cfd4fe199886b538f72976db3a
junhyupa <junhyupa@studenr.42seoul.kr>
2023-06-07 21:33:24 +0900

    ex02 work

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 20:35:53 by junhyupa          #+#    #+#             */
+/*   Updated: 2023/06/07 20:59:39 by junhyupa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ) {}
+Account::~Account( void ) {}
+
+int	Account::getNbAccounts( void )
+{
+
+}
+
+int	Account::getTotalAmount( void )
+{
+
+}
+
+int	Account::getNbDeposits( void )
+{
+
+}
+
+int	Account::getNbWithdrawals( void )
+{
+
+}
+
+void	Account::displayAccountsInfos( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+
+}
+
+int		Account::checkAmount( void ) const
+{
+
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
