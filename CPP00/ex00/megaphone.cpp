576ded3bb7bbaeb6f2198bac1dd38788943e3bed
jun <yoseb95@gmail.com>
2023-06-09 15:22:42 +0900

    for eval

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,39 +6,26 @@
 /*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:43:45 by junhyupa          #+#    #+#             */
-/*   Updated: 2023/06/08 18:51:41 by junhyupa         ###   ########.fr       */
+/*   Updated: 2023/06/09 15:22:30 by junhyupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-char	*ToUpper(char *c)
+std::string	converter(std::string av)
 {
-	int	i;
-
-	i = -1;
-	while (c && c[++i])
-	{
-		if (c[i] >= 'a' && c[i] <= 'z')
-			c[i] += 'A' - 'a';
-	}
-	return (c);
+	for(int i = 0; i < av.length(); i++)
+		av[i] = std::toupper(av[i]);
+	return (av);
 }
 
 int	main(int ac, char **av)
 {
-	std::string st(av[1]);
-	
 	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return (0);
-	}
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	while(*++av)
-	{	
-		std::cout << st.ToUpper();
-	}
+		std::cout << converter(*av);
 	std::cout << std::endl;
 	return (0);
 }
