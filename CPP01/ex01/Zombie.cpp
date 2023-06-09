eea235a0ffb463c2704868bd150239295f17d928
junhyupa <junhyupa@studenr.42seoul.kr>
2023-06-09 15:41:11 +0900

    -

--- CPP01/ex01/Zombie.cpp
+++ CPP01/ex01/Zombie.cpp
@@ -6,14 +6,14 @@
 /*   By: junhyupa <junhyupa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 17:19:39 by junhyupa          #+#    #+#             */
-/*   Updated: 2023/06/08 17:26:00 by junhyupa         ###   ########.fr       */
+/*   Updated: 2023/06/09 15:40:57 by junhyupa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie::Zombie() {}
-Zombie::Zombie(std::string name) : name(Zombie) {}
+Zombie::Zombie(std::string name) : name(name) {}
 Zombie::~Zombie() {}
 
 void	Zombie::announce( void )
